5d661605017eec207e95a7a56bf2ec0b7c905ba3
matej <matej.vavrek@m2ms.sk>
2018-09-06 13:05:12 +0200

    3.7 PHP 7.2 support

--- _config.php
+++ _config.php
@@ -1,7 +1,3 @@
 <?php
 
 define('DISPLAY_LOGIC_DIR', basename(dirname(__FILE__)));
-
-// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
-// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
-if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
